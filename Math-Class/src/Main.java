f7d73e47cb749e4fd852816fa04f36cee00e2dac
barberusa2025 <andrew.barberusa2023@gmail.com>
2025-06-26 12:10:50 -0400

    Add files via upload

---
+++ Math-Class/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Print value of PI
+        System.out.println(Math.PI);
+
+        // Print value of e
+        System.out.println(Math.E);
+
+        // Print result of 2 ^ 3
+        double result = Math.pow(2, 3);
+        System.out.println(result);
+
+        // prints absolute value
+        double absValue = Math.abs(-10);
+        System.out.println(absValue);
+
+        // prints result of square root
+        double sqrt = Math.sqrt(16);
+        System.out.println(sqrt);
+
+        // rounds number to the nearest integer
+        double round = Math.round(10.5);
+        System.out.println(round);
+
+        // round up a number
+        double ceil = Math.ceil(10.5);
+        System.out.println(ceil);
+
+        // round down a number
+        double floor = Math.floor(10.5);
+        System.out.println(floor);
+
+        // displays which number is higher
+        double max = Math.max(10, 20);
+        System.out.println(max);
+
+        // displays which number is lower
+        double min = Math.min(10, 20);
+        System.out.println(min);
+
+
+    }
+}
